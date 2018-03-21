f48cf8a48791dc1fa7f9b433b2a398f84691c75f
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-21 11:55:10 +0000

    add release bike test

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,3 +3,10 @@ require 'app'
 describe DockingStation do 
     it  { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation do 
+    it 'releases a bike' do 
+        expect(subject.release_bike)
+    end 
+
+end 
